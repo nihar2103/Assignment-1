edd9dbd70e6b436afdc32d19b8f408d65266831f
NIHAR PARVEEN A <108277276+nihar2103@users.noreply.github.com>
2023-09-27 08:45:36 +0530

    Update duplicate finder.java

--- duplicate finder.java
+++ duplicate finder.java
@@ -2,12 +2,12 @@ import java.util.*;
 public class Main{
 	public static void main(String[] args){
 	    ArrayList<String> str=new ArrayList<>();
-	    str.add("apple");
-	    str.add("banana");
-	    str.add("cherry");
-	    str.add("apple");
-	    str.add("banana");
-	    str.add("date");
+	    str.add("\"apple\"");
+	    str.add("\"banana\"");
+	    str.add("\"cherry\"");
+	    str.add("\"apple\"");
+	    str.add("\"banana\"");
+	    str.add("\"date\"");
 	    findDuplicates(str);
 	}
 	public static void findDuplicates(ArrayList<String> list){
